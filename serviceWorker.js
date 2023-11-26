d3b6a815611c6f33325f12d6a42683cc66e51c04
usman_rizwan <usmanrizwan771@gmail.com>
2023-11-26 18:59:04 +0500

    updated

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,11 +17,5 @@ self.addEventListener("install", installEvent => {
     )
 })
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
-})
+
 
